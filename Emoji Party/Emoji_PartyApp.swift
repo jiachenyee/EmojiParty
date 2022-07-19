3efeda2dd7aac4b7fe480a631f275004ca1d32e5
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-07-19 11:16:34 +0800

    Initial Commit

---
+++ Emoji Party/Emoji_PartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_PartyApp.swift
+//  Emoji Party
+//
+//  Created by Jia Chen Yee on 19/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_PartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
